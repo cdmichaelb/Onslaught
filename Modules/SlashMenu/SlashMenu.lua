d84c785134e85b66cb22d8f9e7dcf964d69e16a7
Dw <onslaught.dw@gmail.com>
2019-10-17 12:43:07 -0400

    basic slash menu

--- Modules/SlashMenu/SlashMenu.lua
+++ Modules/SlashMenu/SlashMenu.lua
@@ -10,7 +10,7 @@ local function makeFrame(name, excludeParent)
     local frame = CreateFrame("Frame", nil, UIParent)
     frame.anchor = CreateFrame("Frame", nil, frame)
     frame.anchor:SetPoint("TOPLEFT", 32, -16)
-    frame.anchor:SetSize(InterfaceOptionsFramePanelContainer:GetWidth()-64, 1)
+    frame.anchor:SetSize(InterfaceOptionsFramePanelContainer:GetWidth()-32, 1)
     frame.title = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
     frame.title:SetPoint("TOPLEFT", 16, -16)
     frame.title:SetText(format("%s |cff33eeff%s|r", addonName, name))
@@ -19,31 +19,28 @@ local function makeFrame(name, excludeParent)
     end
     frame.name = name
 
-    local contentFrame = AceGUI:Create("Frame")
-    contentFrame.frame.SetParent(frame)
-    contentFrame:SetFullHeight(true)
-    contentFrame:SetFullWidth(true)
-    contentFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 16, -32)
-    local instanceTrackerFrameBackground = contentFrame.frame:CreateTexture()
-    instanceTrackerFrameBackground:SetAllPoints(contentFrame.frame)
+    local contentFrame = CreateFrame("Frame", nil, frame)
+    contentFrame:SetSize(InterfaceOptionsFramePanelContainer:GetWidth()-32, InterfaceOptionsFramePanelContainer:GetWidth()-64)
+    contentFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 16, -48)
+    local instanceTrackerFrameBackground = contentFrame:CreateTexture()
+    instanceTrackerFrameBackground:SetAllPoints(contentFrame)
     instanceTrackerFrameBackground:SetColorTexture(1, 0, 0)
-    contentFrame.frame.texture = instanceTrackerFrameBackground
+    contentFrame.texture = instanceTrackerFrameBackground
 
-    return frame
+    InterfaceOptions_AddCategory(frame)
+
+    return contentFrame, frame
 end
 
-local frameMain = makeFrame("General", true)
-
-local frameLootListImporter = makeFrame("Loot List Importer")
-
+local frameMain = nil
 local function setupSlashMenu()
-    InterfaceOptions_AddCategory(frameMain)
-    InterfaceOptions_AddCategory(frameLootListImporter)
+    _, frameMain = makeFrame("General", true)
+    makeFrame("Loot List Importer")
 end
 
 function OnslaughtAce:OpenSlashMenuOptions(input)
-    InterfaceOptionsFrame_OpenToCategory(frameLootListImporter)
-    InterfaceOptionsFrame_OpenToCategory(frameLootListImporter)
+    InterfaceOptionsFrame_OpenToCategory(frameMain)
+    InterfaceOptionsFrame_OpenToCategory(frameMain)
     InterfaceOptionsFrame_OpenToCategory(frameMain)
 end
 
