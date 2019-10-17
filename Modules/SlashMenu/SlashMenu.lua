863647796d56ebc4ce9138c697c985a06b46af40
Dw <onslaught.dw@gmail.com>
2019-10-17 12:00:16 -0400

    basic slash menu

--- Modules/SlashMenu/SlashMenu.lua
+++ Modules/SlashMenu/SlashMenu.lua
@@ -12,12 +12,21 @@ local function makeFrame(name, excludeParent)
     frame.anchor:SetPoint("TOPLEFT", 32, -16)
     frame.anchor:SetSize(InterfaceOptionsFramePanelContainer:GetWidth()-64, 1)
     frame.title = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
-    frame.title:SetPoint("TOPLEFT", 18, -16)
+    frame.title:SetPoint("TOPLEFT", 16, -16)
     frame.title:SetText(format("%s |cff33eeff%s|r", addonName, name))
     if not excludeParent then
         frame.parent = addonName
     end
     frame.name = name
+
+    local contentFrame = AceGUI:Create("SimpleGroup")
+    contentFrame:SetFullHeight(true)
+    contentFrame:SetFullWidth(true)
+    contentFrame:SetPoint("TOPLEFT", frame, 0, -32)
+    local instanceTrackerFrameBackground = contentFrame.frame:CreateTexture()
+    instanceTrackerFrameBackground:SetAllPoints(contentFrame.frame)
+    instanceTrackerFrameBackground:SetColorTexture(1, 0, 0)
+    contentFrame.frame.texture = instanceTrackerFrameBackground
     return frame
 end
 
