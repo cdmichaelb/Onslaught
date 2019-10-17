de5dbb23260bc4e57632b9996d9b7ea12f20a018
Dw <onslaught.dw@gmail.com>
2019-10-17 13:19:15 -0400

    basic slash menu

--- Modules/SlashMenu/SlashMenu.lua
+++ Modules/SlashMenu/SlashMenu.lua
@@ -7,33 +7,33 @@ if not OSAddon.SlashMenu then
 end
 
 local function makeFrame(name, excludeParent)
+    local height = InterfaceOptionsFramePanelContainer:GetHeight()-64
+    local width = InterfaceOptionsFramePanelContainer:GetWidth()-32
+
     local frame = CreateFrame("Frame", nil, UIParent)
     frame.anchor = CreateFrame("Frame", nil, frame)
     frame.anchor:SetPoint("TOPLEFT", 32, -16)
-    frame.anchor:SetSize(InterfaceOptionsFramePanelContainer:GetWidth()-32, 1)
+    frame.anchor:SetSize(, 1)
     frame.title = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
     frame.title:SetPoint("TOPLEFT", 16, -16)
     frame.title:SetText(format("%s |cff33eeff%s|r", addonName, name))
     if not excludeParent then
         frame.parent = addonName
+        frame.name = name
+    else
+        frame.name = addonName
     end
-    frame.name = name
 
     local contentFrame = CreateFrame("Frame", nil, frame)
-    contentFrame:SetSize(InterfaceOptionsFramePanelContainer:GetWidth()-32, InterfaceOptionsFramePanelContainer:GetWidth()-64)
+    contentFrame:SetSize(width, height)
     contentFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 16, -48)
 
     local contentGroup = AceGUI:Create("SimpleGroup")
-    contentGroup.frame.SetParent(contentFrame)
-    contentGroup.frame.SetPoint("TOPLEFT", contentFrame)
+    contentGroup.frame:SetParent(contentFrame)
+    contentGroup.frame:SetPoint("TOPLEFT", contentFrame)
     contentGroup.frame:Show()
-    contentGroup:SetFullHeight(true)
-    contentGroup:SetFullWidth(true)
-
-    local instanceTrackerFrameBackground = contentGroup.frame:CreateTexture()
-    instanceTrackerFrameBackground:SetAllPoints(contentGroup.frame)
-    instanceTrackerFrameBackground:SetColorTexture(1, 0, 0)
-    contentGroup.frame.texture = instanceTrackerFrameBackground
+    contentGroup:SetHeight(height)
+    contentGroup:SetWidth(width)
 
     InterfaceOptions_AddCategory(frame)
 
@@ -48,7 +48,7 @@ end
 local function makeLootListImporterFrame()
     local contentGroup = makeFrame("Loot List Importer")
     local editBox = AceGUI:Create("MultiLineEditBox")
-    editBox.SetLabel("Import String")
+    editBox:SetLabel("Import String")
     editBox:SetNumLines(20)
     editBox:SetFullWidth(true)
     contentGroup:AddChild(editBox)
